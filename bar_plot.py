4fc1bfc40f13d408d9bc373bf590def051b60327
KyunghoeKim <37864598+KyunghoeKim@users.noreply.github.com>
2018-04-18 15:27:54 +0900

    Update bar_plot.py

--- bar_plot.py
+++ bar_plot.py
@@ -2,64 +2,61 @@
 # Specific format(.csv) required: ask KH for sample data
 # Written by KH Kim, 04/16/18
 
-#def bar_plot(filename = 'Datasheet2', xlabel = 'Group', ylabel = 'Stimulation time (s)'):
-
 import os
 
 import pandas as pd
-import matplotlib as mpl
 import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
 
-# Handle variables
-filename = 'Datasheet2'
-xlabel = 'Group'
-ylabel = 'Distance moved (cm)'
-expGroupName = 'ChR2'
-ctrlGroupName = 'eYFP'
 
-xtickLabel = [expGroupName, ctrlGroupName]
-pos = np.arange(len(xtickLabel))
 
-# Initialize file information
-dirname = "C:\\1\\"
-ext = '.csv'
-filename = os.path.join(dirname, filename + ext)
+def read_all_csv_files(search_dir=None, ext='.csv'):
+    work_dir = search_dir or os.getcwd()
+    filenames = [f for f in os.listdir(work_dir) if f.endswith(ext)]
+    for filename in filenames:
+        df = pd.read_csv(os.path.join(work_dir, filename))
+        for colname in df.columns:
+            if df[colname].dtype == np.float64:
+                ycol = colname
+            elif df[colname].dtype == np.object:
+                # convert string column into categorical type
+                df[colname] = df[colname].astype('category')
+                xcol = colname
+        # return dataframe as well as filename (for saving figure), xcol and ycol
+        yield (df, filename, xcol, ycol)
 
-# Read csv file and classify
-df = pd.read_csv(filename)
-df_exp = df.loc[df[xlabel] == expGroupName]
-df_ctrl = df.loc[df[xlabel] != expGroupName]
+def barplot(df, filename, xcol, ycol, *, colors):
+    fig, ax = plt.subplots(figsize=(3, 5))
+    fig.dpi = 200
+    
+    x = df[xcol]
+    y = df[ycol]
+    data = []
+    for i, category in enumerate(x.cat.categories):
+        y_cat = y[x == category]
+        estimated_mean = y_cat.mean()
+        estimated_std = y_cat.std() / (len(y_cat) - 1)
+        data.append([estimated_mean, 0., estimated_std])
 
-# Statistics
-mean_exp = df_exp[ylabel].mean()
-mean_ctrl = df_ctrl[ylabel].mean()
+    # Convert data to np array and transpose it (.T part) 
+    data = np.array(data).T
 
-std_exp = df_exp[ylabel].std()
-std_ctrl = df_ctrl[ylabel].std()
+    # Use category string value directily for x values (instead of np.arange)
+    ax.bar(x.cat.categories, data[0],
+           color=colors,
+           yerr=data[1:],
+           error_kw=dict(
+               elinewidth=2,
+               capsize=8,
+               capthick=1.5))
+    sns.swarmplot(x=xcol, y=ycol, data=df, color='black')
 
-totalNumMouse = df.shape[0]
-expNumMouse = df_exp.shape[0]
-ctrlNumMouse = df_ctrl.shape[0]
+    # disable right and top axis spine (plot frame)
+    ax.spines['right'].set_visible(False)
+    ax.spines['top'].set_visible(False)
 
-ste_exp = std_exp / np.sqrt(expNumMouse)
-ste_ctrl = std_ctrl / np.sqrt(ctrlNumMouse)
-
-# For plot
-means = [mean_exp, mean_ctrl]
-stes = np.array([[0, 0], [ste_exp, ste_ctrl]])
-
-# Plot
-fig, ax = plt.subplots(figsize = (3, 5))
-ax.bar(pos, means, color = [(0, 168/255, 1), '0.3'],
-       yerr = stes,
-       error_kw = dict(elinewidth = 2, capsize = 8, capthick = 1.5))
-sns.swarmplot(x = xlabel, y = ylabel, data = df, color = 'black')
-
-ax.set_xticks(pos)
-ax.set_xticklabels(xtickLabel)
-ax.spines['right'].set_visible(False)
-ax.spines['top'].set_visible(False)
-fig.show()
-
+# always wrap scripts within `if __name__ == '__main__':`
+if __name__ == '__main__':
+    for args in read_all_csv_files():
+        barplot(*args, colors=[(0, 168/255, 1), '0.3'])
